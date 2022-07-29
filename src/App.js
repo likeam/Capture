42ed323ae993cea84631fbc75f87ca33b95b27a5
AbdulRehman <likeam99@gmail.com>
2022-07-30 01:08:58 +0500

    creating the Nav Section and styling it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
-
+import ContactUs from "./pages/ContactUs";
+import OurWorks from "./pages/OurWorks";
+import {Switch, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <AboutUs />
       <GlobalStyle />
+      <Nav />
+      <Routes>
+      <Route path="/">
+        <AboutUs />
+      </Route>
+      <Route path="/contact">    
+        <ContactUs />
+      </Route>  
+      <Route path="/works">
+        <OurWorks />
+      </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactUs = () => {
+    return (
+        <div>
+        <h1>Contact Our</h1>
+        </div>
+    )
+}
+
+export default ContactUs
